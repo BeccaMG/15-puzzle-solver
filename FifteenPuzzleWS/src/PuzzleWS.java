575c916e37d8113f5e2ad8dd8f020077562f3ca4
Juan Mejia Rojas <juanro.mejia@gmail.com>
2016-02-05 18:22:17 +0100

    Adding interface function

--- FifteenPuzzleWS/src/PuzzleWS.java
+++ FifteenPuzzleWS/src/PuzzleWS.java
@@ -3,40 +3,47 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 import java.util.Arrays;
- 
+
+import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+
 @Path("/puzzle")
 public class PuzzleWS {
  
     @GET
     @Path("/newShuffled")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String newShuffled(@QueryParam("n") Integer n) {
+    @Produces("application/json")
+    public int[] newShuffled(@QueryParam("n") Integer n) {
         Puzzle puzzle = new Puzzle(n);
         puzzle.shuffle(200*n);
-        return Arrays.toString(puzzle.puzzle_array);
+        System.out.println(puzzle.toString());
+        return puzzle.puzzle_array;
     }
  
     @GET
     @Path("/isSolvable")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String isSolvable(@QueryParam("puzzle") String puzzle) {
+    @Produces("application/json")
+    public boolean isSolvable(@QueryParam("puzzle") String puzzle) {
         String puzzleString[]  = puzzle.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", "") .split(","); 
         int puzzleSeed[] = new int[puzzleString.length];
         for (int i = 0; i < puzzleString.length; i++){
             try {
             puzzleSeed[i] = Integer.parseInt(puzzleString[i]);
             } catch (NumberFormatException nfe) {
-                return "ERROR -- Puzzle is not parsable";
+                return false;
             }
         }
         Puzzle npuzzle;
         try {
         npuzzle = new Puzzle(puzzleSeed);
         } catch (ArrayIndexOutOfBoundsException nfe) {
-            return "ERROR - Puzzle is not valid";        
+            return false;        
             }
-        return String.valueOf(npuzzle.isSolvable());
+        return npuzzle.isSolvable();
     }
     
     @GET
@@ -47,9 +54,15 @@ public class PuzzleWS {
     }
  
     @GET
-    @Path("/nextStep")
-    @Produces(MediaType.TEXT_XML)
-    public String sub(@QueryParam("a") double a, @QueryParam("b") double b) {
-        return "<?xml version=\"1.0\"?>" + "<result>" +  (a - b) + "</result>";
+    @Path("/interface")
+    @Produces("text/html")
+    public File sub(@QueryParam("a") double a, @QueryParam("b") double b) {
+      
+     File file = new File("resources/interface.html");
+
+     return file;
     }
+
+
+
 }
