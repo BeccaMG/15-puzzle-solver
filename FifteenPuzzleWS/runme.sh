03fe299d403f40ac318b85a3d389bc634a89cbe7
juanmejiar <juanro.mejia@gmail.com>
2016-02-09 01:54:38 +0100

    Removing unnecessary jars from WS

--- FifteenPuzzleWS/runme.sh
+++ FifteenPuzzleWS/runme.sh
@@ -3,6 +3,7 @@ if [ ! -d bin ] ; then mkdir bin/ ; fi
 javac -cp "jars/*" -sourcepath src -d bin src/*.java
 if [ "$1" == "-d" ] ; then
 sudo nohup java -cp "jars/*:bin"  Publisher &
+if [ $? -eq 0 ] ; then echo "Service running in the background..."; fi
 else
 sudo java -cp "jars/*:bin"  Publisher
 fi
