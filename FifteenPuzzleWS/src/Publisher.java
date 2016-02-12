8b70768b3cde5a13309edf3bc9e2e48070feaaff
Juan Mejia Rojas <juanro.mejia@gmail.com>
2016-02-12 02:52:31 +0100

    Commenting and adding javadocs

--- FifteenPuzzleWS/src/Publisher.java
+++ FifteenPuzzleWS/src/Publisher.java
@@ -3,9 +3,18 @@ import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 import com.sun.net.httpserver.HttpServer;
  
 public class Publisher {
- 
+	/**
+	 * String for the base URI of webservices, change with appropiate hostname and port if neccesary. (Currently uses port 80 and requires sudo permission)
+	 * 
+	 */
     static final String BASE_URI = "http://localhost/";
- 
+    
+    /**
+     * Main function that runs the server
+     * 
+     * Handle IllegalArgumentException and IOException prints them and continue operating.
+     * @param args
+     */
     public static void main(String[] args) {
         try {
             HttpServer server = HttpServerFactory.create(BASE_URI);
--- FifteenPuzzleWS/src/PuzzleWS.java
+++ FifteenPuzzleWS/src/PuzzleWS.java
@@ -10,7 +10,18 @@ import java.io.File;
 
 @Path("/puzzle")
 public class PuzzleWS {
- 
+	
+	/**
+     * Generates a new shuffled puzzle.
+     *
+     * This function of the WS creates a new puzzle shuffle with the standard  
+     * shuffle function of the Puzzle class and respond to the remote client
+     * with the array representation of the puzzle JSONized
+     * e.g. "http://ip:port/puzzle/newShuffled?n=4" Returns a puzzle with dimension
+     * 4.
+     * 	
+     * @param n The dimension of the puzzle grid. Should be > 1. (Fetched from url)
+     */
     @GET
     @Path("/newShuffled")
     @Produces("application/json")
@@ -20,7 +31,14 @@ public class PuzzleWS {
         System.out.println(puzzle.toArray());
         return puzzle.toArray();
     }
- 
+    /**
+     * Verifies if a puzzle is solvable.
+     *
+     * This function receives a puzzle in an array representation verifies if its solvable using
+     * the isSolvable function from puzzle class.
+     * e.g. "http://ip:port/puzzle/isSolvable?puzzle=1,7,10,8,0,3,11,2,5,13,12,15,6,9,14,4" Returns true
+     * @param puzzle The puzzle in array representation (Fetched from url)
+     */
     @GET
     @Path("/isSolvable")
     @Produces("application/json")
@@ -28,6 +46,14 @@ public class PuzzleWS {
         return stringToPuzzle(puzzle).isSolvable();
     }
     
+    /**
+     * Solve the puzzle received
+     * 
+     * This function returns a array with the list of pieces to move to reach the goal state.
+     * Uses the aStar function from solver with fitness function manhattanDistance, the faster
+     * fitness in our tests.
+     * @param puzzle The puzzle in array representation (Fetched from url)
+     */
     @GET
     @Path("/solve")
     @Produces("application/json")
@@ -63,17 +89,32 @@ public class PuzzleWS {
 	    	  
 		return ret;
     }
- 
+    
+    /**
+     * Shows a graphic WebGUI (Beta this was not finished and it only displays a shuffle puzzle)
+     * 
+     * Function to serve the html resource
+     * @return the html file
+     */
     @GET
     @Path("/interface")
     @Produces("text/html")
-    public File sub(@QueryParam("a") double a, @QueryParam("b") double b) {
+    public File serveInterface() {
       
      File file = new File("resources/interface.html");
 
      return file;
     }
     
+    /**
+     * Function that parse a puzzle from a string
+     * 
+     * This functions receives the string fetched from the URL removes the unwanted characters
+     * and returns a clean puzzle.
+     * 
+     * @param puzzleStr Puzzle in a string representation with an array like structure
+     * @return puzzle created from the string
+     */
     public Puzzle stringToPuzzle(String puzzleStr) {
     	Puzzle npuzzle = null;
     	String puzzleString[]  = puzzleStr.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", "") .split(",");
